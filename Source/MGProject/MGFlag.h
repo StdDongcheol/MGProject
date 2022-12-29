56c5a5cf5fd97dcfc96018bd5d0906c844808bdb
local-JODONGCHEOL <highcore444@gmail.com>
2022-12-29 22:42:22 +0900

    feat: Player ActionState의 추가 - QAiming, RAiming 상태 추가

--- Source/MGProject/MGFlag.h
+++ Source/MGProject/MGFlag.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "UObject/NoExportTypes.h"
 #include "MGFlag.generated.h"
 
@@ -21,13 +22,41 @@ enum class ECharacter_ActionState : uint8
 {
 	None,
 	Normal,
-	Aiming
+	Aiming,
+	QAiming,
+	RAiming
 };
 
 UENUM()
-enum class ECharacter_ActiveState
+enum class ECharacter_ActiveState : uint8
 {
 	None,
 	Active,
 	Dead
+};
+
+UENUM()
+enum class EObject_Force : uint8
+{
+	None,
+	Player,
+	Enemy,
+	Neutural
+};
+
+USTRUCT(Atomic, BlueprintType)
+struct FBulletTable : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UParticleSystem* ProjectileEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UParticleSystem* HitEffect;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EObject_Force Force;
+
 };
