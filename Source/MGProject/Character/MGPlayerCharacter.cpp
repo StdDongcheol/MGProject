37637785d5f297bc70969d1bdadc2d496cc03145
local-JODONGCHEOL <highcore444@gmail.com>
2022-12-31 21:30:05 +0900

    feat: Targeting 기능을 위한 컴포넌트 및 콜백함수 추가 - SceneComponent, BoxCollider 추가 - Overlap Begin, End 콜백함수 추가 - Enemy Profile과 충돌시 TArray내 대상 Actor 추가

--- Source/MGProject/Character/MGPlayerCharacter.cpp
+++ Source/MGProject/Character/MGPlayerCharacter.cpp
@@ -3,11 +3,21 @@
 
 #include "MGPlayerCharacter.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/BoxComponent.h"
 
 AMGPlayerCharacter::AMGPlayerCharacter()
 {
 	Capsule->SetCollisionProfileName(FName("Player"));
 
+	BoxRoot = CreateDefaultSubobject<USceneComponent>("QAimSceneComponent");
+	BoxRoot->SetupAttachment(RootComponent);
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>("QBoxColliderComponent");
+	BoxCollision->SetupAttachment(BoxRoot);
+	BoxCollision->SetCollisionProfileName(FName("PlayerAttack"));
+
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &AMGPlayerCharacter::OnCollisionEnter);
+	BoxCollision->OnComponentEndOverlap.AddDynamic(this, &AMGPlayerCharacter::OnCollisionEnd);
 }
 
 void AMGPlayerCharacter::StateUpdate(float DeltaTime)
@@ -23,6 +33,13 @@ void AMGPlayerCharacter::StateUpdate(float DeltaTime)
 	GetAnimInst()->SetMovementYaw(RotValue.Yaw);
 	GetAnimInst()->SetAimRotation(AimRot);
 
+	// Set QDetectBox rotation to Yaw AimRotation.
+	FRotator BoxRot = FRotator(0.0f, AimRot.Yaw, 0.0f);
+	BoxRoot->SetRelativeRotation(BoxRot);
+
+	// Debugging QDetectBox
+	if (BoxCollision && BoxRoot)
+		DrawDebugBox(GetWorld(), BoxCollision->GetComponentLocation(), BoxCollision->GetScaledBoxExtent(), BoxRoot->GetComponentRotation().Quaternion(), FColor::Green);
 }
 
 void AMGPlayerCharacter::BeginPlay()
@@ -30,7 +47,12 @@ void AMGPlayerCharacter::BeginPlay()
 	Super::BeginPlay();
 
 	ArmSpring = FindComponentByClass<USpringArmComponent>();
+	BoxCollision = FindComponentByClass<UBoxComponent>();
+	ParticleQEffect = FindComponentByClass<UParticleSystemComponent>();
+
+	BoxCollision->SetBoxExtent(FVector(256.0f, 128.0f, 128.0f));
 }
+
 
 void AMGPlayerCharacter::Tick(float DeltaTime)
 {
@@ -40,4 +62,29 @@ void AMGPlayerCharacter::Tick(float DeltaTime)
 void AMGPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AMGPlayerCharacter::OnCollisionEnter(UPrimitiveComponent* _pComponent, AActor* _pOtherActor, UPrimitiveComponent* _OtherComp, int32 _OtherBodyIndex, bool _bFromSweep, const FHitResult& _Hit)
+{
+	UCapsuleComponent* Component = _pOtherActor->FindComponentByClass<UCapsuleComponent>();
+	
+	if (!Component || !Component->IsValidLowLevel())
+		return;
+
+	FName CollisionName = Component->GetCollisionProfileName();
+
+	// Target acquired
+	if (CollisionName == TEXT("Enemy"))
+	{
+		TargetArray.Add(_pOtherActor);
+	}
+}
+
+void AMGPlayerCharacter::OnCollisionEnd(UPrimitiveComponent* _pComponent, AActor* _pOtherActor, UPrimitiveComponent* _OtherComp, int32 _OtherBodyIndex)
+{
+	UCapsuleComponent* Component = _pOtherActor->FindComponentByClass<UCapsuleComponent>();
+
+	if (!Component || !Component->IsValidLowLevel())
+		return;
+	
 }
--- Source/MGProject/Character/MGPlayerCharacter.h
+++ Source/MGProject/Character/MGPlayerCharacter.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "MGCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "MGPlayerCharacter.generated.h"
 
 /**
@@ -21,10 +22,30 @@ public:
 private:
 	USpringArmComponent* ArmSpring;
 
+private:
+	TArray<AActor*> TargetArray;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	class UBoxComponent* BoxCollision;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	class USceneComponent* BoxRoot;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UParticleSystemComponent* ParticleQEffect;
+
 public:
 	USpringArmComponent* GetSpringArmComponent() const
 	{
 		return ArmSpring;
+	}
+	
+public:
+	UParticleSystemComponent* GetQEffect() const
+	{
+		return ParticleQEffect;
 	}
 
 protected:
@@ -37,5 +58,16 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+protected:
+	UFUNCTION()
+	void OnCollisionEnter(UPrimitiveComponent* _pComponent, AActor* _pOtherActor, UPrimitiveComponent* _OtherComp,
+		int32 _OtherBodyIndex, bool _bFromSweep, const FHitResult& _Hit);
+
+	UFUNCTION()
+	void OnCollisionEnd(UPrimitiveComponent* _pComponent, AActor* _pOtherActor, UPrimitiveComponent* _OtherComp,
+		int32 _OtherBodyIndex);
+
 
 };
