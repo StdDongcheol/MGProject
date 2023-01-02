a7ad597990bd74aa3bc362f048b5e66fe04f1a40
local-JODONGCHEOL <highcore444@gmail.com>
2023-01-02 22:56:09 +0900

    feat: Aim WidgetAnimation 및 AnimationDelegate 관련 함수 추가 - WidgetAnimation의 Start Delegate로 콜백함수 등록 - 위젯 Visibility 상태에 따라 WidgetAnimation를 활성

--- Source/MGProject/UI/MGAimWidget.cpp
+++ Source/MGProject/UI/MGAimWidget.cpp
@@ -13,9 +13,26 @@ void UMGAimWidget::NativePreConstruct()
 
 void UMGAimWidget::NativeConstruct()
 {
+	ActivateScopeStartDelegate.BindDynamic(this, &UMGAimWidget::ScopeActivateStart);
+
+	BindToAnimationStarted(ActivateScope, ActivateScopeStartDelegate);
+
+	PlayAnimation(DecoAnimLoop_1, 0.0f, 0);
+	PlayAnimation(DecoAnimLoop_2, 0.0f, 0);
 }
 
 void UMGAimWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 {
 	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	if (Visibility == ESlateVisibility::Visible)
+	{
+		if (!ScopeActivated)
+			PlayAnimation(ActivateScope);
+	}
+}
+
+void UMGAimWidget::ScopeActivateStart()
+{
+	SetScopeActivate(true);
 }
--- Source/MGProject/UI/MGAimWidget.h
+++ Source/MGProject/UI/MGAimWidget.h
@@ -6,21 +6,46 @@
 #include "MGUserWidget.h"
 #include "MGAimWidget.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class MGPROJECT_API UMGAimWidget : public UMGUserWidget
 {
 	GENERATED_BODY()
 
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetAnim), Transient)
+	class UWidgetAnimation* ActivateScope;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetAnim), Transient)
+	class UWidgetAnimation* DecoAnimLoop_1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetAnim), Transient)
+	class UWidgetAnimation* DecoAnimLoop_2;
+
+private:
+	FWidgetAnimationDynamicEvent ActivateScopeStartDelegate;
+
+private:
+	bool	ScopeActivated;
+
+public:
+	void SetScopeActivate(bool bEnable)
+	{
+		ScopeActivated = bEnable;
+	}
+
+	bool IsScopeActivated() const
+	{
+		return ScopeActivated;
+	}
 
 protected:
 	virtual void NativeOnInitialized() override;
 	virtual void NativePreConstruct() override;
 	virtual void NativeConstruct() override;
 	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
-	//virtual void Tick(FGeometry MyGeometry, float InDeltaTime) override;
-	
-	
+
+private:
+	UFUNCTION()
+	void ScopeActivateStart();
 };
