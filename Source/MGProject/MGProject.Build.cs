16083f3769f59f914ece663e3ed6f2808a09f81a
local-JODONGCHEOL <highcore444@gmail.com>
2023-01-03 22:54:49 +0900

    fix: 엔진내 Niagara 경로 추가 및 gitignore 수정

--- Source/MGProject/MGProject.Build.cs
+++ Source/MGProject/MGProject.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class MGProject : ModuleRules
 {
@@ -8,9 +9,11 @@ public class MGProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Niagara" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicIncludePaths.AddRange(new string[] { Path.Combine(ModuleDirectory, "Engine/Plugins/FX/Niagara/Source/Niagara") });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
