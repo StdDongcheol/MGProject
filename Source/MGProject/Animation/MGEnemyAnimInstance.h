a193adc9dc8dbe0b6b6438ed50d025aaf59a37af
local-JODONGCHEOL <highcore444@gmail.com>
2023-02-02 22:28:07 +0900

    feat: 적 Animation을 BT에서 제어하기 위한 enum 추가 - EAIAnimState 변수 추가

--- Source/MGProject/Animation/MGEnemyAnimInstance.h
+++ Source/MGProject/Animation/MGEnemyAnimInstance.h
@@ -21,6 +21,21 @@ protected:
 	UPROPERTY(Category = "EnemyState", EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	bool	IsAttacking;
 
+	UPROPERTY(Category = "EnemyState", EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	bool	IsSpawned;
+	
+	UPROPERTY(Category = "EnemyState", EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	bool	IsStunned;
+	
+	UPROPERTY(Category = "EnemyState", EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	bool	IsTurning;
+
+	UPROPERTY(Category = "EnemyState", EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	EAIAnimState	AnimState;
+	
+	UPROPERTY(Category = "EnemyState", EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	bool	TurnTo;
+
 	// Get Functions
 public:
 	bool GetCurrentAttacking() const
@@ -28,11 +43,54 @@ public:
 		return IsAttacking;
 	}
 
+	EAIAnimState GetAIAnimState() const
+	{
+		return AnimState;
+	}
+
+	bool IsStunning() const
+	{
+		return IsStunned;
+	}
+	
+	bool IsSpawnCompleted() const
+	{
+		return IsSpawned;
+	}
+	
+
 	// Set Functions
 public:
 	void SetCurrentAttacking(bool bEnable)
 	{
 		IsAttacking = bEnable;
+	}
+
+	void SetStun(bool bEnable)
+	{
+		IsStunned = bEnable;
+	}
+
+	// True : To right
+	// False : To reft
+	void SetTurning(bool bEnable)
+	{
+		IsTurning = bEnable;
+	}
+
+	void SetTurnTo(bool bEnable)
+	{
+		TurnTo = bEnable;
+	}
+
+	void SetSpawned(bool bEnable)
+	{
+		IsSpawned = bEnable;
+	}
+
+	void SetAIAnimState(EAIAnimState State)
+	{
+		AnimState = State;
 	}
 
 public:
