34f2a8cdcfefe9fa78aa4acbe93be31d461af3e1
local-JODONGCHEOL <highcore444@gmail.com>
2023-03-05 23:20:56 +0900

    feat: LobbyWidget 클래스 추가 - LobbyLevel 추가에 따른 위젯 클래스 추가

---
+++ Source/MGProject/UI/MGLobbyWidget.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MGLobbyWidget.h"
+
+void UMGLobbyWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
+
+void UMGLobbyWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+}
+
+void UMGLobbyWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UMGLobbyWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+}
