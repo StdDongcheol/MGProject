7e74977cee7701c55fe01fe5018ebfe0688d9077
local-JODONGCHEOL <highcore444@gmail.com>
2023-03-09 23:19:43 +0900

    feat: PlayLevel내 GameMode 추가 - BGM 교체 기능 추가

---
+++ Source/MGProject/MGPlayGameMode.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MGPlayGameMode.h"
+#include "MGBlueprintFunctionLibrary.h"
+#include "MGGameInstance.h"
+#include "Components/AudioComponent.h"
+
+AMGPlayGameMode::AMGPlayGameMode()
+{
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("BGMAudio"));
+	RootComponent = AudioComponent;
+	
+	NextBGMSound = nullptr;
+}
+
+void AMGPlayGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	USoundBase* BGMSound = UMGBlueprintFunctionLibrary::GetMGGameInstance()->GetBGMData(TEXT("PlayNormal"));
+	
+	if (!BGMSound || !BGMSound->IsValidLowLevel())
+	{
+		UE_LOG(LogTemp, Error, TEXT("AMGPlayGameMode::BeginPlay() : BGMSound is null!"));
+		return;
+	}
+
+	AudioComponent->SetSound(BGMSound);
+	AudioComponent->Play();
+}
+
+void AMGPlayGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!AudioComponent->IsPlaying() && NextBGMSound)
+	{
+		AudioComponent->SetSound(NextBGMSound);
+		AudioComponent->FadeIn(0.2f, 1.0f, BGMPlayTime);
+
+		NextBGMSound = nullptr;
+	}
+}
+
+void AMGPlayGameMode::ChangeBGM(FName _BGMName, float _FadeDuration)
+{
+	USoundBase* BGMSound = UMGBlueprintFunctionLibrary::GetMGGameInstance()->GetBGMData(_BGMName);
+
+	if (!BGMSound || !BGMSound->IsValidLowLevel())
+	{
+		UE_LOG(LogTemp, Error, TEXT("AMGPlayGameMode::ChangeBGM(FName) : BGMSound is null!"));
+		return;
+	}
+
+	if (_FadeDuration == 0.0f)
+	{
+		AudioComponent->SetSound(BGMSound);
+	}
+
+	else
+	{
+		AudioComponent->FadeOut(_FadeDuration, 0.0f);
+
+		if (UWorld* World = GetWorld())
+		{
+			BGMPlayTime = World->GetAudioTimeSeconds();
+		}
+
+		BGMPlayTime += _FadeDuration;
+		NextBGMSound = BGMSound;
+	}
+}
---
+++ Source/MGProject/MGPlayGameMode.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MGPlayGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MGPROJECT_API AMGPlayGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AMGPlayGameMode();
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Event Setting", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UAudioComponent>	AudioComponent;
+
+private:
+	float	BGMPlayTime;
+	TObjectPtr<class USoundBase> NextBGMSound;
+
+public:
+	UAudioComponent* GetBGMAudioComponent() const
+	{
+		return AudioComponent;
+	}
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	
+public:
+	void ChangeBGM(FName _BGMName, float _FadeDuration = 0.0f);
+};
